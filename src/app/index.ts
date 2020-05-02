69a0979a1acfab3680692cdc5dc4d52dd26c5819
Daniel Noon <supercookies@outlook.com>
2020-05-01 19:47:19 -0500

    finish slideshow

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,47 +1,47 @@
-import mapboxgl, { Map } from 'mapbox-gl';
-import { State } from './State';
-import { slides } from './slides';
-import { wait } from './util';
-import { Slide } from './Slide';
-import { playEntranceAnimations } from './animation';
+import mapboxgl, { Map } from "mapbox-gl";
+import { State } from "./State";
+import { slides } from "./slides";
+import { wait } from "./util";
+import { Slide } from "./Slide";
+import { playEntranceAnimations } from "./animation";
 
 mapboxgl.accessToken =
-  'pk.eyJ1Ijoic3VwZXJtZWdhZGV4IiwiYSI6ImNqNnc4c242NDFjcG0zMm56MzlqMDk1czMifQ.gFotKrTtsriSfvGxKVzsoA';
+  "pk.eyJ1Ijoic3VwZXJtZWdhZGV4IiwiYSI6ImNqNnc4c242NDFjcG0zMm56MzlqMDk1czMifQ.gFotKrTtsriSfvGxKVzsoA";
 
 const map = new Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/satellite-v9',
+  container: "map",
+  style: "mapbox://styles/mapbox/satellite-v9",
   center: [-100, 40],
   zoom: 3,
 });
 
 const state = new State(
   map,
-  document.querySelector<HTMLDivElement>('#bg1')!,
-  document.querySelector<HTMLDivElement>('#bg2')!
+  document.querySelector<HTMLDivElement>("#bg1")!,
+  document.querySelector<HTMLDivElement>("#bg2")!
 );
 
 console.log(state);
 
-const hash = parseInt(location.hash.substring(1) || '-1');
+const hash = parseInt(location.hash.substring(1) || "-1");
 
 let currentSlide = isNaN(hash) ? -1 : hash;
 
 (async () => {
-  document.querySelectorAll<HTMLDivElement>('.slide').forEach((slide) => {
-    slide.classList.add('hidden');
-    slide.style.setProperty('display', 'none');
+  document.querySelectorAll<HTMLDivElement>(".slide").forEach((slide) => {
+    slide.classList.add("hidden");
+    slide.style.setProperty("display", "none");
   });
 
   await wait(200);
 
-  document.querySelector('#loading')?.classList.add('hidden');
+  document.querySelector("#loading")?.classList.add("hidden");
 
   await wait(200);
 
   document
-    .querySelector<HTMLDivElement>('#loading')
-    ?.setAttribute('style', 'display: none');
+    .querySelector<HTMLDivElement>("#loading")
+    ?.setAttribute("style", "display: none");
 
   changeSlides(currentSlide, true);
 })();
@@ -50,8 +50,11 @@ async function changeSlides(index: number, forward: boolean) {
   const slide = slides[index];
   if (!slide) return;
 
-  if (slide.slide.getAttribute('data-background-image')) {
-    slide.slide.style.setProperty('background-image', `url(${slide.slide.getAttribute('data-background-image')}`);
+  if (slide.slide.getAttribute("data-background-image")) {
+    slide.slide.style.setProperty(
+      "background-image",
+      `url(${slide.slide.getAttribute("data-background-image")}`
+    );
   }
 
   currentSlide = index;
@@ -60,11 +63,11 @@ async function changeSlides(index: number, forward: boolean) {
     const prevSlide = slides[index - 1];
     if (prevSlide) {
       await prevSlide.moveFrom(prevSlide.slide, state, slide);
-      prevSlide.slide.classList.add('hidden');
+      prevSlide.slide.classList.add("hidden");
     }
     // await wait(300);
-    slide.slide.classList.remove('hidden');
-    slide.slide.style.setProperty('display', '');
+    slide.slide.classList.remove("hidden");
+    slide.slide.style.setProperty("display", "");
     playEntranceAnimations(slide.slide);
     slide.moveTo(slide.slide, state, prevSlide);
     // if (prevSlide) {
@@ -75,11 +78,11 @@ async function changeSlides(index: number, forward: boolean) {
     const prevSlide = slides[index + 1];
     if (prevSlide) {
       await prevSlide.moveFrom(prevSlide.slide, state, slide);
-      prevSlide.slide.classList.add('hidden');
+      prevSlide.slide.classList.add("hidden");
     }
     await wait(300);
-    slide.slide.classList.remove('hidden');
-    slide.slide.style.setProperty('display', '');
+    slide.slide.classList.remove("hidden");
+    slide.slide.style.setProperty("display", "");
     playEntranceAnimations(slide.slide);
     slide.moveTo(slide.slide, state, prevSlide);
     // if (prevSlide) {
@@ -89,22 +92,31 @@ async function changeSlides(index: number, forward: boolean) {
   }
 }
 
-document
-  .querySelector<HTMLButtonElement>('#next')
-  ?.addEventListener('click', () => {
-    changeSlides(currentSlide + 1, true);
-  });
+// document
+//   .querySelector<HTMLButtonElement>("#next")
+//   ?.addEventListener("click", () => {
+//     changeSlides(currentSlide + 1, true);
+//   });
 
-document
-  .querySelector<HTMLButtonElement>('#previous')
-  ?.addEventListener('click', () => {
+// document
+//   .querySelector<HTMLButtonElement>("#previous")
+//   ?.addEventListener("click", () => {
+//     changeSlides(currentSlide - 1, false);
+//   });
+
+addEventListener("keydown", (ev) => {
+  if (ev.key === "ArrowRight") {
+    changeSlides(currentSlide + 1, true);
+  }
+  if (ev.key === "ArrowLeft") {
     changeSlides(currentSlide - 1, false);
-  });
+  }
+});
 
 window.onhashchange = function () {
-  const hash = parseInt(location.hash.substring(1) || '-1');
+  const hash = parseInt(location.hash.substring(1) || "-1");
 
-  if (!isNaN(hash)) {
-    changeSlides(hash, true);
+  if (!isNaN(hash) && hash != currentSlide) {
+    changeSlides(hash, hash > currentSlide);
   }
 };
--- src/app/slides.ts
+++ src/app/slides.ts
@@ -1,5 +1,5 @@
-import { Slide } from './Slide';
-import { wait } from './util';
+import { Slide } from "./Slide";
+import { wait } from "./util";
 
 function id<T extends HTMLElement>(id: string) {
   return document.querySelector<T>(id)!;
@@ -7,113 +7,116 @@ function id<T extends HTMLElement>(id: string) {
 
 export const slides = [
   new Slide(
-    id<HTMLDivElement>('#slide-1'),
+    id<HTMLDivElement>("#slide-1"),
     (slide, state, next) => {
       state.setMapCenter([138, 32], 4);
     },
     (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-section-islands'),
+    id<HTMLDivElement>("#slide-section-islands"),
     (slide, state, next) => {
       state.setMapCenter([138, 36], 4.5);
     },
     (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-hokkaido'),
+    id<HTMLDivElement>("#slide-hokkaido"),
     (slide, state, next) => {
       state.setMapCenter([142, 43], 6);
     },
     (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-honshu'),
+    id<HTMLDivElement>("#slide-honshu"),
     (slide, state, next) => {
       state.setMapCenter([138, 36], 5);
     },
     (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-kyushu'),
+    id<HTMLDivElement>("#slide-kyushu"),
     (slide, state, next) => {
       state.setMapCenter([131, 33], 7);
     },
     (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-shikoku'),
+    id<HTMLDivElement>("#slide-shikoku"),
     (slide, state, next) => {
       state.setMapCenter([133.5, 33.75], 7);
     },
     (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-section-geography'),
+    id<HTMLDivElement>("#slide-section-geography"),
     (slide, state, next) => {},
     (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-physical-geography'),
-    (slide, state, next) => {
-    },
-    (slide, state, next) => {
-    }
+    id<HTMLDivElement>("#slide-physical-geography"),
+    (slide, state, next) => {},
+    (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-climate-variety'),
+    id<HTMLDivElement>("#slide-climate-variety"),
     (slide, state, next) => {
-      state.showBackground('assets/mountains.jpg');
+      state.showBackground("assets/mountains.jpg");
     },
-    (slide, state, next) => {
-    }
+    (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-seismic-activity'),
+    id<HTMLDivElement>("#slide-seismic-activity"),
     (slide, state, next) => {
-      state.showBackground('assets/tsunami.jpg');
+      state.showBackground("assets/tsunami.jpg");
     },
-    (slide, state, next) => {
-    }
+    (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-volcanism'),
+    id<HTMLDivElement>("#slide-volcanism"),
     (slide, state, next) => {
-      state.showBackground('assets/sakurajima.jpg');
+      state.showBackground("assets/sakurajima.jpg");
     },
-    (slide, state, next) => {
-    }
+    (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-section-culture'),
+    id<HTMLDivElement>("#slide-section-culture"),
     (slide, state, next) => {
-      state.showBackground('assets/torii.jpg');
+      state.showBackground("assets/torii.jpg");
     },
-    (slide, state, next) => {
-    }
+    (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-section-fuji'),
+    id<HTMLDivElement>("#slide-section-fuji"),
     async (slide, state, next) => {
       state.setMapCenter([138.731111, 35.358056], 13, 5000);
       await wait(5250);
-      state.showBackground('assets/fuji-aerial.jpg');
+      state.showBackground("assets/fuji-aerial.jpg");
     },
-    (slide, state, next) => {
-    }
+    (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-fuji'),
-    async (slide, state, next) => {
-    },
-    (slide, state, next) => {
-    }
+    id<HTMLDivElement>("#slide-fuji"),
+    async (slide, state, next) => {},
+    (slide, state, next) => {}
   ),
   new Slide(
-    id<HTMLDivElement>('#slide-fuji-art'),
+    id<HTMLDivElement>("#slide-fuji-art"),
+    async (slide, state, next) => {},
+    (slide, state, next) => {}
+  ),
+  new Slide(
+    id<HTMLDivElement>("#slide-section-shinto"),
     async (slide, state, next) => {
+      state.setMapCenter([136.725833, 34.455], 17);
     },
-    (slide, state, next) => {
-    }
+    (slide, state, next) => {}
+  ),
+  new Slide(
+    id<HTMLDivElement>("#slide-shinto"),
+    async (slide, state, next) => {
+      state.setMapCenter([136.725833, 34.455], 17);
+    },
+    (slide, state, next) => {}
   ),
 ];
